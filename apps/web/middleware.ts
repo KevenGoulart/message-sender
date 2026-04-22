9d33f8523af5c26da492373adbb15e21783dc7b9
keven <kevengoulartmm@gmail.com>
2026-04-22 10:38:47 -0300

    middleware

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -4,8 +4,6 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   const session = request.cookies.get("token-MS")?.value;
 
-  console.log(session);
-
   const pathname = request.nextUrl.pathname;
 
   const protectedRoutes = ["/single", "/group"];
